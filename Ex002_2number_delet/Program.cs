74e38a76bda9bc6be232a024644e6093f0b3de0c
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-08-10 20:51:44 +0500

    Ex002 solved

--- Ex002_2number_delet/Program.cs
+++ Ex002_2number_delet/Program.cs
@@ -1,22 +1,5 @@
 ﻿// Программа, которая принимает на вход три числа и выдаёт максимальное из этих чисел
-Console.WriteLine("Введите три различных числа, и мы выведем максимальное среди них");
-Console.Write("number1 =");
-double number1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("number2 =");
-double number2 = Convert.ToDouble(Console.ReadLine());
-Console.Write("number3 =");
-double number3 = Convert.ToDouble(Console.ReadLine());
-double max = number1;
-if (number2 > max)
-    {
-     max = number2;
-    }
-else
-    {
-        if (number3 > max)
-        {
-         max = number3;
-        }
-    }   
-Console.Write("Максимальное из этих чисел равно  ");
-Console.WriteLine(max);
+Console.Write("Введите трёхзначное число, и мы выведем его без второй цифры, number = ");
+int number = Convert.ToInt32(Console.ReadLine());
+int num = (number / 100) * 10 + number % 10;
+Console.WriteLine($"Было число {number}, а стало {num}");
