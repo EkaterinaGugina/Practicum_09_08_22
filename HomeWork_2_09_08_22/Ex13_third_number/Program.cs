b8e4fe1cde66a10e5ec73d6a9d2db1416f3baa85
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-08-11 01:43:42 +0500

    add Ex13

--- HomeWork_2_09_08_22/Ex13_third_number/Program.cs
+++ HomeWork_2_09_08_22/Ex13_third_number/Program.cs
@@ -1,17 +1,15 @@
 ﻿// Программа, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-Console.WriteLine("Введите целое число, и мы выведем его третью цифру. Number = ");
-string? number_str = Convert.ToString(Console.ReadLine());
-int length = number_str.Length;
-if (length > 2)
-{
-  Console.WriteLine($"Третья цифра числа {number_str} равна {number_str[2]}.");
-}
-else 
-{
-  Console.WriteLine($"В числе {number_str} нет третьей цифры.");  
-}
-
-Concole.Cleare;
+//Console.WriteLine("Введите целое число, и мы выведем его третью цифру. Number = ");
+//string? number_str = Convert.ToString(Console.ReadLine());
+//int length = number_str.Length;
+//if (length > 2)
+//{
+ // Console.WriteLine($"Третья цифра числа {number_str} равна {number_str[2]}.");
+//}
+//else 
+//{
+//  Console.WriteLine($"В числе {number_str} нет третьей цифры.");  
+//}
 
 // Второй способ (без использования типа "строка")
 
@@ -25,11 +23,10 @@ if (number > 99)
     while (num1 > 100)     //считаем "длину" числа
     {
         num1 /= 10;
-        deg = deg * 10;
+        deg  *= 10;
     }
-        num1 = (number - num1 * deg) / (deg / 10);  //делим на (deg / 10), т.к. первую цифру полученного в цикле числа (начинающегося с третьей цифры исходного числа), хотим оставить (т.к. количество цифр deg и num1 одинаковое)
-
-    Console.WriteLine($"Третья цифра числа {number} равна {num1}.");
+        num1 = (number - num1 * deg) / (deg / 10);  //делим на (deg / 10), т.к. первую цифру полученного в цикле числа, хотим оставить (количество цифр deg и num1 одинаковое)
+        Console.WriteLine($"Третья цифра числа {number} равна {num1}.");
 }
 else 
 {
