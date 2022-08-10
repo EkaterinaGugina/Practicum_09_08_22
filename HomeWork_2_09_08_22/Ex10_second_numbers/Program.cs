2d720594313b2b04d117fb97197a222d9fb53258
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-08-10 21:56:52 +0500

    Example N10 solved

--- HomeWork_2_09_08_22/Ex10_second_numbers/Program.cs
+++ HomeWork_2_09_08_22/Ex10_second_numbers/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Это программа, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Write("Введите трёхзначное число, и мы выведем его вторую цифру, number = ");
+int number = Convert.ToInt32(Console.ReadLine());
+int num = (number / 10) % 10;
+Console.WriteLine($"Ввели число {number}, а его вторая цифра равна {num}");
